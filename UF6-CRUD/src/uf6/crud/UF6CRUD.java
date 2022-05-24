09fcbc205ef6687353540df93f3067bafe3938b5
Xavi <Xavi@DESKTOP-606OICB>
2022-05-24 17:26:14 +0200

    Cambios de Xavi 1

--- UF6-CRUD/src/uf6/crud/UF6CRUD.java
+++ UF6-CRUD/src/uf6/crud/UF6CRUD.java
@@ -11,6 +11,25 @@ public class UF6CRUD {
         con.consultaSelect("SELECT * FROM persones");
         con.consultaSelect2("SELECT * FROM instruments");
         
+        con.consultaIUD("INSERT INTO persones VALUES ('12345678C','Alex','78945')");
+        con.consultaIUD("INSERT INTO instruments VALUES ('3','guitarra','roja','12345678C')");
+        
+       
+        //con.consultaIUD("DELETE FROM persones WHERE nom = 'Alex'");
+        
+       // con.consultaIUD("DELETE FROM instruments WHERE n_serie= 1");
+        
+       //con.consultaIUD("DELETE FROM instruments WHERE color= 'marron'");
+        
+       con.consultaSelect("SELECT * FROM persones WHERE dni= '12345678C'");
+       
+       con.consultaSelect2("SELECT * FROM instruments WHERE n_serie = 1");
+              
+        con.consultaSelect("SELECT * FROM persones");
+        con.consultaSelect2("SELECT * FROM instruments");
+        
+        
+        
         con.tancarConnexio();
     }
     
